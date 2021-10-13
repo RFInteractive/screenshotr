542c4cccbffd8211a5311a90615e779aa7cb3897
Jamin <jaminwebdev@gmail.com>
2021-10-13 09:33:53 -0700

    api route logic refactoring

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -10,10 +10,11 @@ const handler = nc().get(async (_, res) => {
   try {
     checkForScreenshotDirectory();
     clearScreenshotDirectory();
+    createSiteDirectories(websites);
     await screenshotSites(websites);
     res.status(200).end('complete')
   } catch(error) {
-    res.status(500).end(err.toString());
+    res.status(500).end(error.toString());
   }
 
 });
@@ -32,25 +33,22 @@ const clearScreenshotDirectory = () => {
   fs.rmdirSync(path.join(process.cwd(), './public/static/screenshots/'), { recursive: true });
 }
 
+const createSiteDirectories = (sites) => {
+  sites.forEach( site => 
+    fs.mkdirSync(path.join(process.cwd(), `./public/static/screenshots/${site.directoryName}`), { recursive: true })
+  );
+  return;
+}
+
 const screenshotSites = async (sites) => {
 
   return await Promise.all(sites.map( async(site) => {
-    createSiteDirectory(site.directoryName);
     await screenshotUrls(site.urls, site.directoryName);
   }))
 
 }
 
-const createSiteDirectory = (directory) => {
-    fs.mkdirSync(path.join(
-      process.cwd(), 
-      `./public/static/screenshots/${directory}`), 
-      { recursive: true }
-    );
-    return;
-}
-
-const screenshotUrls = async (urls, directory) => {
+const screenshotUrls = async (urls, directory) => { // [promise, promise, promise] => Promise({resolved, err, resolved, resolved, err})
     return await Promise.all(urls.map( url => {
       return captureWebsite.file(
         url.url, path.join(process.cwd(), 
